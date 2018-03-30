7e629ea1b1ce6afba023e5737631da8cf4374745
wdz1222 <wdz1222@126.com>
2018-03-30 10:39:00 +0800

    修改了部分bug

--- LTM/LTM.py
+++ LTM/LTM.py
@@ -3,17 +3,21 @@ import numpy as np
 
 class LTM:
 
-    def __init__(self, graph_path, influencer_path, iter_num):
+    def __init__(self, graph_path, influencer_path, iter_num, seeds_num):
         self.G = nx.read_gpickle(graph_path)
         self.n = self.G.number_of_nodes()
         self.iter_num = iter_num
+        self.seeds_num = seeds_num
         self.diffusivity_ratio = np.zeros(self.iter_num, dtype=np.float)
         self.influence_index = self.init_influence_index(influencer_path)
 
     def init_influence_index(self, influencer_path):
-        influencer = np.loadtxt(influencer_path, dtype=int)
+        influencer = np.loadtxt(influencer_path, dtype=np.int)
+        if len(influencer.shape) != 1:
+            influencer = influencer[:, 0]
+        print(influencer)
         influence_index = np.zeros(self.n)
-        influence_index[influencer] = 1
+        influence_index[influencer[0:self.seeds_num]] = 1
         return influence_index
 
     def LTM_algorithm(self, active_threshold):
@@ -30,13 +34,22 @@ class LTM:
                         influence_index_temp.append(i)
             self.influence_index[influence_index_temp] = 1
             self.diffusivity_ratio[j] = sum(self.influence_index)/self.n
-            print(self.influence_index)
+            # print(self.influence_index)
         print(self.diffusivity_ratio)
 
 
 if __name__ == "__main__":
-    graph_path = '../graph_data/er_graph_n50_p0.2.gpickle'
-    influencer_path = '../influencer_data/er_graph_n50_p0_influencer_2.txt'
-    ltm = LTM(graph_path, influencer_path, 50)
-    ltm.LTM_algorithm(0.1)
+    # graph_path = '../graph_data/er_graph_n50_p0.2.gpickle'
+    # influencer_path = '../influencer_data/er_graph_n50_p0_influencer_3.txt'
+    # influencer_path = '../other_data/er_graph_n50_p0_closeness.txt'
+    # influencer_path = '../other_data/er_graph_n50_p0_degree_centrality.txt'
+    # influencer_path = '../other_data/er_graph_n50_p0_eigenvector_centrality.txt'
+
+    graph_path = '../graph_data/netscience_n1589_p2742.gpickle'
+    # influencer_path = '../influencer_data/netscience_n1589_p2742_influencer_3.txt'
+    # influencer_path = '../other_data/netscience_n1589_p2742_closeness.txt'
+    # influencer_path = '../other_data/netscience_n1589_p2742_degree_centrality.txt'
+    influencer_path = '../other_data/netscience_n1589_p2742_eigenvector_centrality.txt'
+    ltm = LTM(graph_path, influencer_path, 50, 500)
+    ltm.LTM_algorithm(0.2)
     print(ltm.influence_index)
--- graphs/Graph.py
+++ graphs/Graph.py
@@ -25,6 +25,22 @@ class Graph:
         nx.draw(self.G, pos=nx.spring_layout(self.G), node_size=10, width=0.3, node_color=label, with_labels= False)
         plt.show()
 
+    @staticmethod
+    def paint_compare(compare_path):
+        compare = np.loadtxt(compare_path)
+        x = range(51)
+        plt.figure(1)
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        plt.plot(x, compare[0, :], linestyle='-')
+        plt.plot(x, compare[1, :], linestyle='--')
+        plt.plot(x, compare[2, :], linestyle=':')
+        plt.plot(x, compare[3, :], marker='o', linestyle='-', markersize=3)
+        plt.plot(x, compare[4, :], marker='^', linestyle='-', markersize=3)
+        plt.plot(x, compare[5, :], marker='*', linestyle='-', markersize=3)
+        label = ["union_order=1", "union_order=2", "union_order=3", u"接近度中心性", u"度中心性", u"特征向量中心性"]
+        plt.legend(label, loc=1, ncol=1)
+        plt.show()
+
     def paint_eig(self, threshold_path1, threshold_path2, threshold_path3):
         ts1 = np.loadtxt(threshold_path1)
         ts2 = np.loadtxt(threshold_path2)
@@ -55,15 +71,13 @@ if __name__ == "__main__":
     # threshold_path3 = '../influencer_data/er_graph_n50_p0_eig_3.txt'
     # graph.paint_eig(threshold_path1, threshold_path2, threshold_path3)
 
-    graph_path = '../graph_data/netscience_n1589_p2742.gpickle'
-    graph = Graph(graph_path)
-    influencer_path = '../influencer_data/netscience_n1589_p2742_influencer_3.txt'
-    threshold_path = '../influencer_data/netscience_n1589_p2742_eig_3.txt'
-    threshold = 0.3617
-    graph.paintGraph(influencer_path, threshold_path, threshold)
+    # graph_path = '../graph_data/netscience_n1589_p2742.gpickle'
+    # graph = Graph(graph_path)
+    # influencer_path = '../influencer_data/netscience_n1589_p2742_influencer_3.txt'
+    # threshold_path = '../influencer_data/netscience_n1589_p2742_eig_3.txt'
+    # threshold = 0.3617
+    # graph.paintGraph(influencer_path, threshold_path, threshold)
 
-    # threshold_path1 = '../influencer_data/netscience_n1589_p2742_eig_1.txt'
-    # threshold_path2 = '../influencer_data/netscience_n1589_p2742_eig_2.txt'
-    # threshold_path3 = '../influencer_data/netscience_n1589_p2742_eig_3.txt'
-    # threshold = [0.0471, 0.217496, 0.3617]
-    # graph.paint_eig1(threshold_path1, threshold_path2, threshold_path3, threshold)
+    # compare_path = '../LTM/netscience_compare.txt'
+    compare_path = '../LTM/er_compare.txt'
+    Graph.paint_compare(compare_path)
